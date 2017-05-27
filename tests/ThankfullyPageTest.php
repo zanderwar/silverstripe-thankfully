00a0c029302a1ecdd41862325467758691d0c463
Reece <reece@steadlane.com.au>
2017-05-27 18:33:22 +1000

    Blah

--- tests/ThankfullyPageTest.php
+++ tests/ThankfullyPageTest.php
@@ -12,6 +12,14 @@ class ThankfullyPageTest extends SapphireTest
         /** @var ThankfullyPage $page */
         $page = $this->objFromFixture('ThankfullyPage', 'default_page');
 
-        $this->assertTrue(($page->URLSegment == 'thank-you'));
+        $queryString = array(
+            'k' => 'v',
+            'z' => 'w'
+        );
+
+        $page->setQueryString($queryString);
+
+        $this->assertTrue((Session::get('Thankfully.PageQueryString') == $queryString), 'The query string was not saved to session correctly');
+        $this->assertTrue(strstr('?k=v&z=w', $page->Link()), 'The query string was not appended to the link');
     }
 }
