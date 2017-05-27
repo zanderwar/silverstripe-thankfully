b2a461bea2f284509201f0418adfb266d477f9fa
Reece <reece@steadlane.com.au>
2017-05-27 19:01:41 +1000

    More blah

--- tests/ThankfullyPageTest.php
+++ tests/ThankfullyPageTest.php
@@ -16,10 +16,17 @@ class ThankfullyPageTest extends SapphireTest
             'k' => 'v',
             'z' => 'w'
         );
-
         $page->setQueryString($queryString);
-
         $this->assertTrue((Session::get('Thankfully.PageQueryString') == $queryString), 'The query string was not saved to session correctly');
-        $this->assertContains('?k=v&z=w', $page->Link(), 'The query string was not appended to the link');
+        $this->assertContains(http_build_query($queryString), $page->Link(), 'The query string was not appended to the link');
+
+        $page->setPageTitle('Thank You! :)');
+        $this->assertEquals('Thank You! :)', Session::get('Thankfully.PageTitle'), 'The page title was not saved to session correctly');
+
+        $page->setPageContent('<p>Hello World</p>');
+        $this->assertEquals('<p>Hello World</p>', Session::get('Thankfully.PageContent'), 'The page content was not saved to session correctly');
+
+        $page->setReturnTo('/my/cool/page');
+        $this->assertEquals('/my/cool/page', Session::get('Thankfully.PageReturnTo'), 'The return to URL was not saved to session correctly');
     }
 }
